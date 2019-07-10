xiaomi-sdm660/android_device_xiaomi_platina

wayne: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: I7f06edb331a2a56f29db26294b2ece28666d2103
Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

AndroidProducts.mk
@@ -24,3 +24,8 @@
 # inherit MI 6X HAVOC device config
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/havoc_wayne.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_wayne-eng \
+    havoc_wayne-userdebug \
+		havoc_wayne-user

coderzstas/android_device_xiaomi_wayne

AndroidProducts.mk
@@ -24,3 +24,9 @@
 # inherit MI 6X AICP device config
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_wayne.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_wayne-eng \
+    aicp_wayne-userdebug \
+		aicp_wayne-user
+